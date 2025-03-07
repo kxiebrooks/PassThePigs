26a8bb26159ce2f2c9d9d25624fd758adc4de141
kxiebrooks <150973302+kxiebrooks@users.noreply.github.com>
2025-03-07 15:51:58 +0000

    fixing the spacing

--- .vscode/src/main/PassThePig.java
+++ .vscode/src/main/PassThePig.java
@@ -16,9 +16,9 @@ class PassThePig{
         while(true){
             while(GenericBot.wantsToRoll(GenericBot.getScore(),handScore, otherScores, winningScore)&&canRoll&&(GenericBot.getScore()+handScore)<100){
                     roll = pigs.roll();
-                    if (roll==0){
-                        canRoll =false; 
-                        handScore =0;
+                    if (roll == 0){
+                        canRoll = false; 
+                        handScore = 0;
                     }
                     else{
                         handScore += roll;
@@ -26,13 +26,13 @@ class PassThePig{
                     }
                 }
             GenericBot.addScore(handScore);
-            handScore=0;
-            canRoll=true;
+            handScore = 0;
+            canRoll = true;
             if (GenericBot.getScore()>=100){
                 System.out.println("Winner is GenericBot");
                 break;
             }
-            for(int i=0; i<players.size(); i++){
+            for(int i = 0; i < players.size(); i++){
                 if (players.get(i) instanceof BotKev){
                     BotKev bot = (BotKev) players.get(i);
                     System.out.print("| " + bot.getName() + ": " + bot.getScore() +" |");
@@ -46,26 +46,26 @@ class PassThePig{
             }
             while(canRoll&&Kev.wantsToRoll()){
                 roll = pigs.roll();
-                if (roll==0){
+                if (roll == 0){
                     System.out.println("You lost all the points in the hand");
-                    canRoll =false; 
-                    handScore =0;
+                    canRoll = false; 
+                    handScore = 0;
                 }
                 else{
                     handScore += roll;
-                    System.out.println("You rolled " + roll + " points" + "Your hold " + handScore + "in your hand. You currently have " + (handScore + Kev.getScore()) + " scores");
+                    System.out.println("You rolled " + roll + " points" + "Your hold " + handScore + " in your hand. You currently have " + (handScore + Kev.getScore()) + " scores");
                 }
             }
             Kev.addScore(handScore);
-            handScore=0;
-            canRoll=true;
-            if (Kev.getScore()>=100){
+            handScore = 0;
+            canRoll = true;
+            if (Kev.getScore() >= 100){
                 System.out.println("Winner is Kev");
                 break;
             }
         }
                 
-        for(int i=0; i<players.size(); i++){
+        for(int i = 0; i < players.size(); i++){
             if (players.get(i) instanceof BotKev){
                 BotKev bot = (BotKev) players.get(i);
                 System.out.print("| " + bot.getName() + ": " + bot.getScore() +" |");
@@ -99,7 +99,7 @@ class Player{
 }
 
 class Human extends Player{
-    int myScore = 0 ;
+    int myScore = 0;
     Scanner scanner = new Scanner(System.in);
     public Human(String name){
         super(name);
@@ -148,8 +148,8 @@ class BotKev extends Player{
     }
     public int getHighestScores(ArrayList<Integer> otherScores){
         int highestScore = 0;
-        for (int i=0; i< otherScores.size(); i++){
-            if (otherScores.get(i)>highestScore){
+        for (int i = 0; i< otherScores.size(); i++){
+            if (otherScores.get(i) > highestScore){
                 highestScore = otherScores.get(i);
             }
         } 
@@ -157,24 +157,23 @@ class BotKev extends Player{
     }
 
     public boolean strategyRisky(){
-        if((winningScore - highestScore) <= 10){
+        if((winningScore - highestScore) <= 15){
             return true;
         }
-        else if(handScore>=10){
+        else if(handScore >= 10){
             return false;
         }
         else if ((winningScore - myScore - handScore) <= 10){
             return true;
         }
-        System.out.println(handScore);
         return true;
     }
     public ArrayList<Player> getPlayers(){
         return Player.players;
     }
     public void addScore(int score){
-        myScore+=score;
-        handScore =0;
+        myScore += score;
+        handScore = 0;
     }
 }
 class Pigs{
@@ -185,54 +184,54 @@ class Pigs{
     int reset;
     public int roll(){
         possibility = Math.random();
-        if (possibility<=0.349){
+        if (possibility <= 0.349){
             pig2 = "Dot";
-            score +=1;
+            score += 1;
         }
-        else if (possibility<=0.651){
+        else if (possibility <= 0.651){
             pig2 = "No Dot";
-            score+=1;
+            score += 1;
         }
         else if (possibility<=0.875){
             pig2 = "Razorback";
-            score+=5;
+            score += 5;
         }
-        else if (possibility<=0.963){
+        else if (possibility <= 0.963){
             pig2 = "Trotter";
-            score+=5;
+            score += 5;
         }
-        else if (possibility<=0.993){
+        else if (possibility <= 0.993){
             pig2 = "Snouter";
-            score+=10;
+            score += 10;
         }
         else{
-            pig2 ="Leaning Jowler";
-            score+=15;
+            pig2 = "Leaning Jowler";
+            score += 15;
         }
         possibility = Math.random();
-        if (possibility<=0.349){
+        if (possibility <= 0.349){
             pig2 = "Dot";
-            score +=1;
+            score += 1;
         }
-        else if (possibility<=0.651){
+        else if (possibility <= 0.651){
             pig2 = "No Dot";
-            score+=1;
+            score += 1;
         }
-        else if (possibility<=0.875){
+        else if (possibility <= 0.875){
             pig2 = "Razorback";
-            score+=5;
+            score += 5;
         }
-        else if (possibility<=0.963){
+        else if (possibility <= 0.963){
             pig2 = "Trotter";
-            score+=5;
+            score += 5;
         }
-        else if (possibility<=0.993){
+        else if (possibility <= 0.993){
             pig2 = "Snouter";
-            score+=10;
+            score += 10;
         }
         else{
-            pig2 ="Leaning Jowler";
-            score+=15;
+            pig2 = "Leaning Jowler";
+            score += 15;
         }
         check();
         reset = score;
@@ -242,21 +241,21 @@ class Pigs{
 
     public void check(){
         if (pig1.equals(pig2)){
-            if (score ==2){
-                score =1;
+            if (score == 2){
+                score = 1;
             }
-            else if (score ==10){
-                score =20;
+            else if (score == 10){
+                score = 20;
             }
-            else if (score ==20 ){
-                score =40;
+            else if (score == 20 ){
+                score = 40;
             }
             else{
-                score =60;
+                score = 60;
             }
         }
-        else if(score ==2){
-            score =0;
+        else if(score == 2){
+            score = 0;
         }
     }
 }
